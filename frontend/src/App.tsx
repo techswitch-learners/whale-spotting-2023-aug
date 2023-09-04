cba2f0e8f87e492e5ce6e89252900687068c9478
Akla Djankla <djsolangea@gmail.com>
2023-09-04 16:11:13 +0100

    add SubmissionForm skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SubmissionForm from "./pages/SubmissionForm";
 import "./App.scss";
 
 export const App = () => {
@@ -7,6 +8,7 @@ export const App = () => {
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/SubmissionForm" element={<SubmissionForm />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/SubmissionForm.tsx
@@ -0,0 +1,44 @@
+const SubmissionForm = () => {
+  return (
+    <>
+      <form>
+        <h1>Submit a sighting</h1>
+        <label htmlFor="date">Date of sighting</label>
+        <input type="text" id="date" name="date" />
+
+        <label htmlFor="location">Location</label>
+
+        <label htmlFor="species">Species</label>
+        <select name="species" id="species">
+          <option value="Blue Whale">Blue Whale</option>
+          <option value="Bowhead Whale">Bowhead Whale</option>
+          <option value="Bryde's Whale">Bryde's Whale</option>
+          <option value="False Killer Whale">False Killer Whale</option>
+          <option value="Fin whale">Fin whale</option>
+          <option value="Gray Whale">Gray Whale</option>
+          <option value="Hump Back Whale">Hump Back Whale</option>
+          <option value="Killer whale">Killer whale</option>
+          <option value="Minke Whale">Minke Whale</option>
+          <option value="Pilot Whale">Pilot Whale</option>
+          <option value="Right whale">Right whale</option>
+          <option value="Sei Whale">Sei Whale</option>
+          <option value="Sperm Whale">Sperm Whale</option>
+        </select>
+
+        <label htmlFor="date">Description</label>
+        <textarea id="description" rows={4} cols={50} name="description">
+          {" "}
+        </textarea>
+
+        <form action="/action_page.php">
+          <label htmlFor="date">Upload your image</label>
+          <input type="file" id="myFile" name="filename" />
+        </form>
+
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default SubmissionForm;
