b3a60ad9448f172969f64e9d2286baf3b14ebde9
RBletso <138777363+RBletso@users.noreply.github.com>
2023-09-05 10:39:11 +0100

    navbar complete

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,27 +1,44 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react"; // Import useState from React
+import "./Navbar.scss"; // Import your SCSS file
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false); // State to control mobile menu visibility
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/posts">Posts</NavLink>
-      </li>
-      <li>
-        <NavLink to="/info">Info</NavLink>
-      </li>
-      <li>
-        <NavLink to="/register">Register</NavLink>
-      </li>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-    </ul>
+    <nav>
+      <div className="navbar-wrapper">
+        <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <ul className={`navbar-links ${showMobileMenu ? "active" : ""}`}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/users">Users</NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts">Posts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/info">Info</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
