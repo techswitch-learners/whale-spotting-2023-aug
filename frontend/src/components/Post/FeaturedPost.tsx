e8fc421bc5c937b39c99355f53fbc1174786eedf
Will Gerry <william.gerry@outlook.com>
2023-09-05 16:29:06 +0100

    WS47: Add initial featured post component. Add basic styling. Requires final layout

--- frontend/src/components/Post/FeaturedPost.tsx
+++ frontend/src/components/Post/FeaturedPost.tsx
@@ -10,24 +10,35 @@ interface PostDataProps {
 const FeaturedPost = ({ postData }: PostDataProps) => {
   return (
     <div className="FeaturedPost">
-      <div className="FeaturedPost__banner">
-        <div className="FeaturedPost__banner__likes">
-          <img src="/post_icon.png" alt="whale icon" />
-          <span>{parseLikes(postData.likes)}</span>
-        </div>
-        <div>
-          <img src="/share_icon.png" alt="share post" />
-        </div>
-      </div>
       <img
         className="FeaturedPost__image"
         src={postData.imageUrl}
         alt={`image of ${postData.species}`}
       />
-      <div className="FeaturedPost__info">
-        <p className="FeaturedPost__title">{postData.species}</p>
-        <p className="FeaturedPost__text">{postData.username}</p>
-        <p className="FeaturedPost__text">{postData.sightingDate}</p>
+      <div className="FeaturedPost__content">
+        <div className="FeaturedPost__info">
+          <p className="FeaturedPost__title">{postData.species}</p>
+          <p className="FeaturedPost__text">{postData.sightingDate}</p>
+        </div>
+        <div className="FeaturedPost__user">
+          <div className="FeaturedPost__user__image-container">
+            <img
+              className="FeaturedPost__user__image"
+              src="https://itsnotacareer.files.wordpress.com/2021/12/for-profile.jpg?w=816"
+              alt={`${postData.username}'s profile picture`}
+            />
+          </div>
+          <p className="FeaturedPost__text">{postData.username}</p>
+        </div>
+        <div className="FeaturedPost__interactions">
+          <div className="FeaturedPost__interactions__likes">
+            <img src="/post_icon.png" alt="whale icon" />
+            <span>{parseLikes(postData.likes)}</span>
+          </div>
+          <div>
+            <img src="/share_icon.png" alt="share post" />
+          </div>
+        </div>
       </div>
     </div>
   );
