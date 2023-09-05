e2e4e2f3f47440bc83e4a67e6301ae0f522a9381
unknown <paul7markets@hotmail.com>
2023-09-05 15:24:29 +0100

    WS-45: like icon and share icon. No share functionality

--- frontend/src/components/Post/CardPost.tsx
+++ frontend/src/components/Post/CardPost.tsx
@@ -5,12 +5,36 @@ interface PostDataProps {
   postData: PostData;
 }
 
+const parseLikes = (likes: number) => {
+  if (likes > 1000) {
+    const thousands = Math.floor(likes / 1000);
+    const hundreds = Math.floor((likes / 1000 - thousands) * 10);
+    if (hundreds === 0) {
+      return `${thousands} k`;
+    } else {
+      return `${thousands}.${hundreds} k`;
+    }
+  }
+  return likes.toString();
+};
+
 const CardPost = ({ postData }: PostDataProps) => {
   return (
     <div className="CardPost">
-      <div className="CardPost__image">
-        <img src={postData.imageUrl} alt={`image of ${postData.species}`} />
+      <div className="CardPost__banner">
+        <div className="CardPost__banner__likes">
+          <img src="/post_icon.png" alt="whale icon" />
+          <span>{parseLikes(postData.likes)}</span>
+        </div>
+        <div>
+          <img src="/share_icon.png" alt="share post" />
+        </div>
       </div>
+      <img
+        className="CardPost__image"
+        src={postData.imageUrl}
+        alt={`image of ${postData.species}`}
+      />
       <div className="CardPost__info">
         <p className="CardPost__title">{postData.species}</p>
         <p className="CardPost__text">{postData.username}</p>
--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -6,56 +6,73 @@ export interface PostData {
   species: string;
   username: string;
   sightingDate: string;
+  likes: number;
 }
 
 const postData: PostData[] = [
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/where-to-go-whale-watching-virginia-1522419979.jpg?resize=1200:*",
     species: "humpback",
     username: "Ariel",
     sightingDate: "04 September 2023",
+    likes: 22100,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://www.wildlifeworldwide.com/images/home/whale_watching_grey_baja.jpg",
     username: "Ariel",
     species: "humpback",
     sightingDate: "04 September 2023",
+    likes: 22234,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/where-to-go-whale-watching-virginia-1522419979.jpg?resize=1200:*",
     username: "Nemo",
     species: "humpback",
     sightingDate: "03 September 2023",
+    likes: 22399,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://www.wildlifeworldwide.com/images/home/whale_watching_grey_baja.jpg",
     username: "Aquaman",
     species: "humpback",
     sightingDate: "02 September 2023",
+    likes: 22,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://www.visitnsw.com/sites/visitnsw/files/styles/portrait_320x575/public/2022-05/174916.jpg?h=19ea9127&itok=tp2_HrNx",
     username: "Ariel",
     species: "humpback",
     sightingDate: "28 August 2023",
+    likes: 224,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/where-to-go-whale-watching-virginia-1522419979.jpg?resize=1200:*",
     username: "Ariel",
     species: "humpback",
     sightingDate: "28 August 2023",
+    likes: 22022,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://www.wildlifeworldwide.com/images/home/whale_watching_grey_baja.jpg",
     username: "Nemo",
     species: "humpback",
     sightingDate: "28 August 2023",
+    likes: 22321,
   },
   {
-    imageUrl: "string",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/where-to-go-whale-watching-virginia-1522419979.jpg?resize=1200:*",
     username: "Aquaman",
     species: "humpback",
     sightingDate: "28 August 2023",
+    likes: 22321,
   },
 ];
 
@@ -64,7 +81,7 @@ export const Posts = () => {
     <main>
       <h1>Whale Spotting Posts</h1>
       <section className="section-dark">
-        <div className="section-content">
+        <div className="container">
           <h2>The Whale</h2>
           <p>
             Whales are a widely distributed and diverse group of fully aquatic
@@ -91,7 +108,7 @@ export const Posts = () => {
       </section>
 
       <section className="Section-Two">
-        <div className="section-content PostsGallery">
+        <div className="container PostsGallery">
           {postData.map((post) => {
             return <CardPost postData={post} />;
           })}
