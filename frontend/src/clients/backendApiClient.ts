b6cca9356c76c70b8f3d5754132c2ec68379721e
Akla Djankla <djsolangea@gmail.com>
2023-09-06 16:04:37 +0100

    add Users page

--- frontend/src/clients/backendApiClient.ts
+++ frontend/src/clients/backendApiClient.ts
@@ -1,3 +1,5 @@
+import { UserData } from "../pages/Users";
+
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const checkBackendConnection = async (): Promise<boolean> => {
@@ -7,4 +9,10 @@ export const checkBackendConnection = async (): Promise<boolean> => {
     return false;
   }
   return true;
+};
+
+export const retrieveUsers = async (id: number): Promise<UserData> => {
+  const response = await fetch(`${backendUrl}/User/${id}`);
+
+  return response.json();
 };
