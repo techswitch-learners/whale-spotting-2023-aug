b385b0b3867960ce55dff6415a9ea8c8726091f5
Akla Djankla <djsolangea@gmail.com>
2023-09-07 16:03:54 +0100

    modify Users.tsx and create UserData.ts

---
+++ frontend/src/models/UserData.ts
@@ -0,0 +1,9 @@
+interface UserData {
+  id: number;
+  username: string;
+  email: string;
+  name: string;
+  profileImageUrl: string;
+}
+
+export default UserData;
--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,14 +1,7 @@
 import "../components/Post/CardPost.scss";
 import { retrieveUsers } from "../clients/backendApiClient";
 import CardUser from "../components/CardUser";
-
-export interface UserData {
-  id: number;
-  username: string;
-  email: string;
-  name: string;
-  profileImageUrl: string;
-}
+import UserData from "../models/UserData";
 
 const userData: UserData[] = [];
 for (let i = 1; i < 21; i++) {
