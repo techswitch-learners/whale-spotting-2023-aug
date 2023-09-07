e12fd03386e7885ef7f4955074da70f5f748ba1a
mina <mina.lui@softwire.com>
2023-09-07 12:26:59 +0100

    dynamically update user page with list of users

---
+++ frontend/src/components/CardUser.tsx
@@ -0,0 +1,24 @@
+import { UserData } from "../pages/Users";
+import "./Post/CardPost.scss";
+
+interface UserDataProps {
+  userData: UserData;
+}
+
+const CardUser = ({ userData }: UserDataProps) => {
+  return (
+    <div className="CardPost">
+      <img
+        className="CardPost__image"
+        src={userData.profileImageUrl}
+        alt={`image of ${userData.name}`}
+      />
+      <div className="CardPost__info">
+        <p className="CardPost__title">{userData.name}</p>
+        <p className="CardPost__text">{userData.username}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardUser;
