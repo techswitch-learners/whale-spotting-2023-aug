cde4630c8a5df650bd68450f37074b4f524e6c78
Janisha Patel <Janisha.Patel@softwire.com>
2023-09-13 16:28:51 +0100

    Added Footer Content and Styles

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,65 @@
+import { NavLink } from "react-router-dom";
 import "./Footer.scss";
 
 function Footer() {
   return (
-    <div className="Footer">
-      <h4 className="Footer__Title">Footer</h4>
-      <hr className="Footer__Divider" />
-    </div>
+    <footer className="section-dark">
+      <div className="Footer container">
+        <div className="Footer__Title">
+          <img
+            className="Footer__Title__Logo"
+            src="./logo.png"
+            alt="WhaleSpotting Logo"
+          />
+          <h4>Explore the Whale Spotting site</h4>
+        </div>
+        <div>
+          <ul className="Footer__Links">
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active-page" : "")}
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active-page" : "")}
+                to="/users"
+              >
+                Users
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active-page" : "")}
+                to="/posts"
+              >
+                Posts
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active-page" : "")}
+                to="/map"
+              >
+                Map
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active-page" : "")}
+                to="/login"
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <p className="Copyright">© July 2023 Cohort @ Techswitch</p>
+    </footer>
   );
 }
 
