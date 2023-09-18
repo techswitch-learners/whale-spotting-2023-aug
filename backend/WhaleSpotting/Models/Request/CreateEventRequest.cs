6be664e479d3454e2de3c3fdab4a709b5900d5ee
Akla Djankla <djsolangea@gmail.com>
2023-09-18 14:25:33 +0100

    add data annotations for input data validation

--- backend/WhaleSpotting/Models/Request/CreateEventRequest.cs
+++ backend/WhaleSpotting/Models/Request/CreateEventRequest.cs
@@ -1,11 +1,24 @@
-﻿namespace WhaleSpotting.Models.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhaleSpotting.Models.Request;
 
 public class CreateEventRequest
 {
+    [MinLength(1)]
     public string Name { get; set; } = null!;
+
+    [DataType(DataType.DateTime)]
     public DateTime StartDate { get; set; }
+
+    [Range(0, int.MaxValue)]
     public int DurationInHours { get; set; }
+
+    [MinLength(2)]
     public string Location { get; set; } = null!;
+
+    [Url]
     public string Link { get; set; } = null!;
+
+    [Url]
     public string ImageUrl { get; set; } = null!;
 }
--- backend/WhaleSpotting/Models/Request/CreatePostRequest.cs
+++ backend/WhaleSpotting/Models/Request/CreatePostRequest.cs
@@ -1,12 +1,22 @@
-﻿namespace WhaleSpotting.Models.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhaleSpotting.Models.Request;
 
 public class CreatePostRequest
 {
     public int UserId { get; set; }
+
+    [Range(-90, 90)]
     public double Latitude { get; set; }
+
+    [Range(-180, 180)]
     public double Longitude { get; set; }
     public int? SpeciesId { get; set; }
     public int? WhaleId { get; set; }
+
+    [Url]
     public string ImageUrl { get; set; } = null!;
+
+    [MinLength(5)]
     public string Description { get; set; } = null!;
 }
