dd54d882f6b6bc3684059e92aaabc5afe5196aa7
Arda Kurt <to.arda.kurt@gmail.com>
2023-09-18 14:28:59 +0100

    CSS Fix

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -8,6 +8,7 @@ import WhaleLoader from "../components/UI/WhaleLoader";
 import Button from "../components/UI/Button";
 import SearchResultCard from "../components/Search/SearchResultCard";
 import BodyOfWaterData from "../models/BodyOfWaterData";
+import "./SearchResult.scss";
 
 const SearchResult = () => {
   const [bodyOfWater, setBodyOfWater] = useState<BodyOfWaterData>();
