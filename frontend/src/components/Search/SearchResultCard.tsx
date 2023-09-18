e4dbca921a2524682a1cbfe48872838c1b351016
Arda Kurt <to.arda.kurt@gmail.com>
2023-09-18 15:03:11 +0100

    CSS Changes

--- frontend/src/components/Search/SearchResultCard.tsx
+++ frontend/src/components/Search/SearchResultCard.tsx
@@ -1,47 +1,48 @@
 import { toShortDate } from "../../utils/DateConversion";
 import { Link } from "react-router-dom";
 import postIcon from "../../assets/post_icon.png";
-import shareIcon from "../../assets/share_icon.png";
-import SearchPostData from "../../models/SearchPostData";
+import ShareButtonExpandable from "../ShareButtonExpandable";
+import PostData from "../../models/PostData";
 import "./SearchResultCard.scss";
+import "../../components/ShareButtonExpandable.scss";
 
 interface Props {
-  post: SearchPostData;
+  post: PostData;
 }
 
 function SearchResultCard({ post }: Props) {
   return (
     <>
-      <Link to={`/posts/${post.id}`}>
-        <div className="SearchResultCard">
+      <div className="SearchResultCard">
+        <Link to={`/posts/${post.id}`}>
           <div className="SearchResultCard__Image">
             <img src={post.imageUrl} alt="" />
           </div>
-          <div className="SearchResultCard__Descriptions">
-            <p>{post.description.slice(0, 150)}...</p>
-            <div className="SearchResultCard__Details">
-              <div className="SearchResultCard__Details--left">
-                <div>
-                  <img
-                    className="SearchResultCard__Details--left--icon"
-                    src={postIcon}
-                    alt="whale icon"
-                  />
-                  <span>{post.rating}</span>
-                </div>
-                <div>
-                  <img
-                    className="SearchResultCard__Details--left--icon"
-                    src={shareIcon}
-                    alt="whale icon"
-                  />
-                </div>
+        </Link>
+        <div className="SearchResultCard__Descriptions">
+          <p>{post.description.slice(0, 150)}...</p>
+          <div className="SearchResultCard__Details">
+            <div className="SearchResultCard__Details--left">
+              <div>
+                <img
+                  className="SearchResultCard__Details--left--icon"
+                  src={postIcon}
+                  alt="whale icon"
+                />
+                <span>{post.rating}</span>
               </div>
+            </div>
+            <div className="SearchResultCard__Details--right">
+              <ShareButtonExpandable
+                postData={post}
+                size={24}
+                type={"sighting"}
+              />
               <p>{toShortDate(post.timestamp)}</p>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 }
