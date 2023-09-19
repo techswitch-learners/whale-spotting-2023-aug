c3d9735c0ffbbbb2de334a0a709f16654b733bb0
Akla Djankla <djsolangea@gmail.com>
2023-09-19 10:21:16 +0100

    add annotation to ApproveOrRejectPostRequest

--- backend/WhaleSpotting/Models/Request/ApproveOrRejectPostRequest.cs
+++ backend/WhaleSpotting/Models/Request/ApproveOrRejectPostRequest.cs
@@ -1,8 +1,10 @@
-﻿using WhaleSpotting.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using WhaleSpotting.Enums;
 
 namespace WhaleSpotting.Models.Request;
 
 public class ApproveOrRejectPostRequest
 {
+    [Range(0, 2)]
     public ApprovalStatus ApprovalStatus { get; set; }
 }
