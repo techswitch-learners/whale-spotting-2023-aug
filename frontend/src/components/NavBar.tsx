05e2e5a57c4f583bd9e1c9c7d331cc21a240d017
RBletso <138777363+RBletso@users.noreply.github.com>
2023-09-20 11:52:09 +0100

    whale logo home

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -36,7 +36,9 @@ function Navbar() {
     <nav>
       <div className="navbar-wrapper">
         <div className="navbar-left">
-          <img src={logo} alt="Whale Icon" className="whale-icon" />
+          <NavLink to="/" onClick={closeMobileMenu}>
+            <img src={logo} alt="Whale Icon" className="whale-icon" />
+          </NavLink>
         </div>
         <div className="navbar-right">
           <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
