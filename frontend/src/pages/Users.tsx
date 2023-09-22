5bc12824e7d597e7df5c0a4dd96c7240efc25363
RBletso <138777363+RBletso@users.noreply.github.com>
2023-09-22 11:28:04 +0100

    button fix

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -2,14 +2,49 @@ import { useState, useEffect } from "react";
 import { getAllUsers } from "../clients/backendApiClient";
 import CardUser from "../components/User/CardUser";
 import UsersData from "../models/UsersData";
+import WhaleLoader from "../components/UI/WhaleLoader";
+import Button from "../components/UI/Button";
 import "../components/Post/CardPost.scss";
 
 export const Users = () => {
   const [usersData, setUsersData] = useState<UsersData>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
-    getAllUsers().then(setUsersData);
+    getAllUsers()
+      .then((data) => {
+        setUsersData(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading || error) {
+    return (
+      <main>
+        <h1>Search for Users</h1>
+        <section className="section-dark">
+          <div className="container Users__loader">
+            <WhaleLoader
+              isLoading={loading}
+              message={
+                loading ? "Loading..." : "Couldn't fetch users at this time."
+              }
+            />
+            {error && (
+              <Button onClick={() => window.location.reload()}>
+                Try Again
+              </Button>
+            )}
+          </div>
+        </section>
+      </main>
+    );
+  }
 
   return (
     <main>
